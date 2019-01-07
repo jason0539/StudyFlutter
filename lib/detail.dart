a15f5b5288a3024e335ca8803029fb5d6486271f
jason0539 <jason0539@163.com>
2019-01-07 15:48:58 +0800

    增加水波纹嵌套布局

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
 class Detail extends StatelessWidget {
+  final imgUrl =
+      "http://gss0.baidu.com/-4o3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/10dfa9ec8a13632796c4c1dd908fa0ec08fac737.jpg";
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("Images"),
         ),
-        body: new Image.network(
-            'http://gss0.baidu.com/-4o3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/10dfa9ec8a13632796c4c1dd908fa0ec08fac737.jpg'),
-      );
+        body: new InkWell(
+          onTap: () {
+            Scaffold.of(context)
+                .showSnackBar(new SnackBar(content: new Text('Tap')));
+          },
+          child: new Container(
+            padding: new EdgeInsets.all(12.0),
+            child: new Image.network(imgUrl),
+          ),
+        ));
   }
 }
