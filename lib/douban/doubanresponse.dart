e29cb103619c8a39a9eefe7bbb48bf52c8aeb821
jason0539 <jason0539@163.com>
2019-01-11 11:37:33 +0800

    复杂数据结构序列化 https://juejin.im/post/5b5f00e7e51d45190571172f

--- lib/douban/doubanresponse.dart
+++ lib/douban/doubanresponse.dart
@@ -3,12 +3,43 @@ import 'package:json_annotation/json_annotation.dart';
 part 'doubanresponse.g.dart';
 
 @JsonSerializable()
+class DImages {
+  @JsonKey(name: "small")
+  final String small;
+  final String large;
+  final String medium;
+
+  DImages(this.small, this.large, this.medium);
+
+  factory DImages.fromJson(Map<String, dynamic> json) =>
+      _$DImagesFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DImagesToJson(this);
+}
+
+@JsonSerializable()
+class DMovie {
+  final String title;
+  final String year;
+  final DImages images;
+  final String id;
+
+  DMovie(this.title, this.year, this.images, this.id);
+
+  factory DMovie.fromJson(Map<String, dynamic> json) => _$DMovieFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DMovieToJson(this);
+}
+
+@JsonSerializable()
 class DoubanResponse {
   final int count;
   final int start;
   final int total;
+  @JsonKey(name: "subjects")
+  final List<DMovie> movies;
 
-  DoubanResponse(this.count, this.start, this.total);
+  DoubanResponse(this.count, this.start, this.total, this.movies);
 
   factory DoubanResponse.fromJson(Map<String, dynamic> json) =>
       _$DoubanResponseFromJson(json);
--- lib/douban/doubanresponse.g.dart
+++ lib/douban/doubanresponse.g.dart
@@ -6,14 +6,49 @@ part of 'doubanresponse.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
+DImages _$DImagesFromJson(Map<String, dynamic> json) {
+  return DImages(json['small'] as String, json['large'] as String,
+      json['medium'] as String);
+}
+
+Map<String, dynamic> _$DImagesToJson(DImages instance) => <String, dynamic>{
+      'small': instance.small,
+      'large': instance.large,
+      'medium': instance.medium
+    };
+
+DMovie _$DMovieFromJson(Map<String, dynamic> json) {
+  return DMovie(
+      json['title'] as String,
+      json['year'] as String,
+      json['images'] == null
+          ? null
+          : DImages.fromJson(json['images'] as Map<String, dynamic>),
+      json['id'] as String);
+}
+
+Map<String, dynamic> _$DMovieToJson(DMovie instance) => <String, dynamic>{
+      'title': instance.title,
+      'year': instance.year,
+      'images': instance.images,
+      'id': instance.id
+    };
+
 DoubanResponse _$DoubanResponseFromJson(Map<String, dynamic> json) {
   return DoubanResponse(
-      json['count'] as int, json['start'] as int, json['total'] as int);
+      json['count'] as int,
+      json['start'] as int,
+      json['total'] as int,
+      (json['subjects'] as List)
+          ?.map((e) =>
+              e == null ? null : DMovie.fromJson(e as Map<String, dynamic>))
+          ?.toList());
 }
 
 Map<String, dynamic> _$DoubanResponseToJson(DoubanResponse instance) =>
     <String, dynamic>{
       'count': instance.count,
       'start': instance.start,
-      'total': instance.total
+      'total': instance.total,
+      'subjects': instance.movies
     };
