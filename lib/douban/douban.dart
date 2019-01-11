af6a420ee30099b2bb548cc1ff9b9df4c78dc7d6
jason0539 <jason0539@163.com>
2019-01-11 17:12:20 +0800

    下拉加载更多

--- lib/douban/douban.dart
+++ lib/douban/douban.dart
@@ -14,8 +14,11 @@ class Douban extends StatefulWidget {
 
 class DoubanState extends State<Douban> {
   DoubanResponse _doubanResponse = null;
+  ScrollController _scrollController = new ScrollController();
+  bool isPerformingRequest = false;
 
   void fetchDouban() async {
+    print("fetchDouban");
     final response = await http
         .get("https://api.douban.com/v2/movie/in_theaters?start=0&count=15");
     Map<String, dynamic> responseMap = json.decode(response.body);
@@ -23,18 +26,49 @@ class DoubanState extends State<Douban> {
     if (responseMap.containsKey('code') && responseMap['code'] != 200) {
       String msg = responseMap['msg'];
       print(msg);
-    }else{
+    } else {
       setState(() {
         _doubanResponse = DoubanResponse.fromJson(responseMap);
       });
     }
   }
 
+  void fetchNextPage() async {
+    print("fetchNextPage");
+    if (!isPerformingRequest) {
+      setState(() {
+        isPerformingRequest = true;
+      });
+      final response = await http
+          .get("https://api.douban.com/v2/movie/in_theaters?start=0&count=15");
+      Map<String, dynamic> responseMap = json.decode(response.body);
+      print(responseMap);
+      setState(() {
+        _doubanResponse.movies
+            .addAll(DoubanResponse.fromJson(responseMap).movies);
+        isPerformingRequest = false;
+      });
+    }
+  }
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        fetchNextPage();
+      }
+    });
+
     fetchDouban();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 
   @override
@@ -46,12 +80,11 @@ class DoubanState extends State<Douban> {
             if (i.isOdd) {
               return new Divider();
             }
-            if (_doubanResponse == null) {
-              return new CircularProgressIndicator();
-            }
             final movies = _doubanResponse.movies;
             final index = i ~/ 2;
-            if (index < movies.length) {
+            if (index == movies.length) {
+              return _buildProgressIndicator();
+            } else {
               return new Row(
                 children: <Widget>[
                   new Image.network(
@@ -64,18 +97,33 @@ class DoubanState extends State<Douban> {
             }
           },
           physics: const AlwaysScrollableScrollPhysics(),
-//                      itemCount: movies.length,
+          itemCount: (_doubanResponse == null)
+              ? 0
+              : _doubanResponse.movies.length * 2 + 1,
+          controller: _scrollController,
         ),
         onRefresh: () {
           fetchDouban();
         });
-    Widget body = _doubanResponse == null ? loading : content;
+    Widget body = (_doubanResponse == null) ? loading : content;
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("DouBan"),
       ),
       body: new Center(
         child: body,
+      ),
+    );
+  }
+
+  Widget _buildProgressIndicator() {
+    return new Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Opacity(
+          opacity: isPerformingRequest ? 1.0 : 0.0,
+          child: new CircularProgressIndicator(),
+        ),
       ),
     );
   }
